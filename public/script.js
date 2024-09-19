aa4902c85687dd97d64edf7270734fcc5f44905f
Sergei <37767713+anaaanyev@users.noreply.github.com>
2024-09-20 01:43:04 +0300

    Delete public directory

--- public/script.js
+++
@@ -1,6 +0,0 @@
-const socket = io();
-
-socket.on('dataUpdate', (data) => {
-    document.getElementById('temperature').innerText = data.temperature;
-    document.getElementById('humidity').innerText = data.humidity;
-});
